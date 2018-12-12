ce6f87e160455685e2860229b344c442f2315502
姜高宇 <1531115737@qq.com>
2018-12-12 12:31:38 +0800

    Initial commit

---
+++ src/main/java/com/sdu/inas/ltp/bean/Arg.java
@@ -0,0 +1,25 @@
+package com.sdu.inas.ltp.bean;
+
+
+import lombok.Data;
+
+@Data
+public class Arg {
+
+    private int id;
+    private String type;
+    private int begin;
+    private int end;
+
+    public Arg(int id, String type, int begin, int end) {
+        this.id = id;
+        this.type = type;
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public Arg() {
+
+    }
+
+}
---
+++ src/main/java/com/sdu/inas/ltp/bean/Word.java
@@ -0,0 +1,27 @@
+package com.sdu.inas.ltp.bean;
+
+
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+public class Word {
+
+    private int id;
+    private String cont;
+    private String pos;
+    private  String ne;
+    private Integer parent;
+    private String relate;
+    private Integer semparent;
+    private String semrelate;
+    private List<Arg> args = new ArrayList<>();
+
+    public Word() {
+    }
+
+}
